21130202eb58f0f8d085b559e60b42b791fe9264
Sofia Solari <125601813+SofiaSol10@users.noreply.github.com>
2025-09-12 08:36:43 -0500

    Update laboratorioCompiladores.cpp
    hice un cambio en la funcion tokenizar

--- laboratorioCompiladores.cpp
+++ laboratorioCompiladores.cpp
@@ -6,16 +6,44 @@
 
 vector<string> tokenizar(const string& texto) {
     vector<string> tokens;
-    stringstream ss(texto);
     string token;
+    string delimiters = " (){}=;<>!";
 
-    while (ss >> token) {
+    for (size_t i = 0; i < texto.size();) {
+        if (isspace(texto[i])) {
+            i++;
+            continue;
+        }
+
+        // Detectar operadores relop de 2 caracteres (==, !=)
+        if (i + 1 < texto.size()) {
+            string twoChars = texto.substr(i, 2);
+            if (twoChars == "==" || twoChars == "!=") {
+                tokens.push_back(twoChars);
+                i += 2;
+                continue;
+            }
+        }
+
+        // Si es delimitador de 1 char
+        if (delimiters.find(texto[i]) != string::npos) {
+            tokens.push_back(string(1, texto[i]));
+            i++;
+            continue;
+        }
+
+        // Tokenizar palabra o número
+        size_t j = i;
+        while (j < texto.size() && delimiters.find(texto[j]) == string::npos && !isspace(texto[j]))
+            j++;
+
+        token = texto.substr(i, j - i);
         tokens.push_back(token);
+        i = j;
     }
 
     return tokens;
 }
-
 int main() {
     std::ifstream archivo("archivo.txt"); 
     if (!archivo.is_open()) {
